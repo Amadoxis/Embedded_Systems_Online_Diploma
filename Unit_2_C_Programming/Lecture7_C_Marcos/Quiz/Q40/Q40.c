fc6e4c6cbeda06ed8b73a14d3f8cc5da834de7f7
Amadoxis <Amadoxis@gmail.com>
2022-10-04 23:09:04 +0200

    Quiz 6

---
+++ Unit_2_C_Programming/Lecture7_C_Marcos/Quiz/Q40/Q40.c
@@ -0,0 +1,25 @@
+/*
+ * Q40.c
+ *
+ *  Created on: Oct 4, 2022
+ *      Author: Ahmed M. Rizk
+ */
+#include <stdio.h>
+
+#define Ahmed m##a##i##n
+
+
+int Ahmed(){
+	float radius;
+
+	printf("\nI cant find the main fuction! Please help!!\n");
+
+	return 0;
+}
+
+
+
+
+
+
+
